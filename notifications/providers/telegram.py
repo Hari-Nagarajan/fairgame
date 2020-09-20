37f04dd1dae516fcdcfcc7274408f3b4c802d842
mihaiparvu <mihaiparvu90@gmail.com>
2020-09-20 12:04:01 +0300

    feat: Add Telegram notifications

---
+++ notifications/providers/telegram.py
@@ -0,0 +1,35 @@
+import json
+import requests
+from os import path
+
+from utils.logger import log
+
+TELEGRAM_CONFIG_PATH = "telegram_config.json"
+TELEGRAM_CONFIG_KEYS = ['BOT_TOKEN', 'BOT_CHAT_ID']
+
+
+class TelegramHandler:
+    enabled = False
+
+    def __init__(self):
+        log.info("Initializing telegram handler")
+
+        if path.exists(TELEGRAM_CONFIG_PATH):
+            with open(TELEGRAM_CONFIG_PATH) as json_file:
+                self.config = json.load(json_file)
+                if self.config['BOT_TOKEN'] and self.config['BOT_CHAT_ID']:
+                    self.bot_token = self.config['BOT_TOKEN']
+                    self.bot_chat_id = self.config['BOT_CHAT_ID']
+                    self.enabled = True
+        else:
+            log.info("No Telegram config found.")
+
+    def send(self, message_body):
+        try:
+            send_text = f'https://api.telegram.org/bot{self.bot_token}/sendMessage?' \
+                        f'chat_id={self.bot_chat_id}&parse_mode=Markdown&text={message_body}'
+            requests.get(send_text)
+        except Exception as e:
+            log.error(e)
+            log.warn("Telegram send message failed. Disabling Telegram notifications.")
+            self.enabled = False
