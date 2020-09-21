cf93786197aa79c9a7aa1a2fff1ca673a0ee4484
Hari-Nagarajan <hari@nagarajan.io>
2020-09-21 08:27:03 -0400

    feat: Make logger generate log files.
    Signed-off-by: Hari-Nagarajan <hari@nagarajan.io>

--- utils/logger.py
+++ utils/logger.py
@@ -1,8 +1,11 @@
 import logging
 
-log = logging.getLogger("Foo")
+log = logging.getLogger("nvidia-bot")
 log.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter('%(levelname)s: "%(asctime)s - %(message)s'))
 
+file_log_handler = logging.FileHandler('nvidia-bot.log')
+file_log_handler.setFormatter(logging.Formatter('%(levelname)s: "%(asctime)s - %(message)s'))
 log.addHandler(handler)
+log.addHandler(file_log_handler)
