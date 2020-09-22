7b63fa4c274f5a96681de92430fd17a39e0b0668
Hari-Nagarajan <hari@nagarajan.io>
2020-09-22 09:53:56 -0400

    bugfix: Ajax clustering problem
    Signed-off-by: Hari-Nagarajan <hari@nagarajan.io>

--- stores/evga.py
+++ stores/evga.py
@@ -76,8 +76,10 @@ class Evga:
 
     def buy(self, delay=5, test=False):
         if test:
-            self.driver.get("https://www.evga.com/products/ProductList.aspx?type=0&family=GeForce+16+Series+Family&chipset=GTX+1660")
-            selenium_utils.wait_for_page(self.driver, "EVGA - Products - Graphics - GeForce 16 Series Family - GTX 1660")
+            self.driver.get(
+                "https://www.evga.com/products/ProductList.aspx?type=0&family=GeForce+16+Series+Family&chipset=GTX+1660")
+            selenium_utils.wait_for_page(self.driver,
+                                         "EVGA - Products - Graphics - GeForce 16 Series Family - GTX 1660")
         else:
             self.driver.get(
                 "https://www.evga.com/products/productlist.aspx?type=0&family=GeForce+30+Series+Family&chipset=RTX+3080"
@@ -118,6 +120,7 @@ class Evga:
         selenium_utils.wait_for_page(self.driver, "EVGA - Checkout - Billing Options")
 
         log.info("Ensure that we are paying with credit card")
+        sleep(1)  # Fix this.
         WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, './/input[@value="rdoCreditCard"]'))
         ).click()
