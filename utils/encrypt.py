46dc4ba599e68c92d725a5a8e3c23de8f07f2f72
Worst Mod Confirmed <wmc@privmail.me>
2020-09-28 03:59:35 -0500

    Bumped scrypt cost to conform with file encryption recommendation.

--- utils/encrypt.py
+++ utils/encrypt.py
@@ -8,16 +8,14 @@ import os
 
 def encrypt(pt, password):
   salt = get_random_bytes(32)
-  key = scrypt(password, salt, key_len=32, N=2**17, r=8, p=1)
-
+  key = scrypt(password, salt, key_len=32, N=2**20, r=8, p=1)
   nonce = get_random_bytes(12)
   cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
-
   ct, tag = cipher.encrypt_and_digest(pt)
-
   json_k = [ 'nonce', 'salt', 'ct', 'tag' ]
   json_v = [ b64encode(x).decode('utf-8') for x in (nonce, salt, ct, tag) ]
   result = json.dumps(dict(zip(json_k, json_v)))
+
   return result
 
 def decrypt(ct, password):
@@ -27,10 +25,9 @@ def decrypt(ct, password):
     json_v = {k:b64decode(b64Ct[k]) for k in json_k}
 
     key = scrypt(password, json_v['salt'], key_len=32, N=2**17, r=8, p=1)
-
     cipher = AES.new(key, AES.MODE_GCM, nonce=json_v['nonce'])
-
     ptData = cipher.decrypt_and_verify(json_v['ct'], json_v['tag'])
+
     return ptData
   except (KeyError, ValueError):
     print("Incorrect Password.")
