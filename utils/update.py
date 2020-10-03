bdf22e0ddebf301710c4a0946c49147319ca8718
Worst Mod Confirmed <wmc@privmail.me>
2020-10-03 15:39:13 -0500

    Testing local commit against remote.

--- utils/update.py
+++ utils/update.py
@@ -7,9 +7,12 @@ proj_url = "https://github.com/Hari-Nagarajan/nvidia-bot.git"
 def get_remote_version():
     threading.Timer(600, get_remote_version).start()
     local_repo = git.Repo(".")
+    local_commit = local_repo.commit()
     fetch = local_repo.remotes.origin.fetch()[0]
-    info = fetch.flags
-    if info is not 4:
-        log.warning(
-          "Version out of date, please pull the latest."
-        )
+    info = fetch.commit
+    print(local_commit)
+    print(info)
+    #if info != 4:
+    #    log.warning(
+    #      "Version out of date, please pull the latest."
+    #    )
