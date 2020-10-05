a40a11998736eea16838035e7c9236305b40dab4
geekyed <eddweston@me.com>
2020-10-05 22:48:17 +0100

    expose previously hidden logs to .log file also.

--- utils/logger.py
+++ utils/logger.py
@@ -2,16 +2,13 @@ import coloredlogs
 import logging
 import os
 
-log = logging.getLogger("nvidia-bot")
-log.setLevel(logging.DEBUG)
-
-file_log_handler = logging.FileHandler("nvidia-bot.log")
-file_log_handler.setFormatter(
-    logging.Formatter('%(levelname)s: "%(asctime)s - %(message)s')
+logging.basicConfig(
+    format='%(levelname)s: "%(asctime)s - %(message)s',
+    filename="nvidia-bot.log",
+    level=logging.DEBUG,
 )
-file_log_handler.setLevel(logging.DEBUG)
 
 LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
 coloredlogs.install(level=LOGLEVEL, fmt='%(levelname)s: "%(asctime)s - %(message)s')
 
-log.addHandler(file_log_handler)
+log = logging.getLogger("nvidia-bot")
