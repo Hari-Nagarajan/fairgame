d3b56c54b2e8a0b2e18716b23ea506cf11e31dad
geekyed <eddweston@me.com>
2020-10-07 20:36:42 +0100

    added back in the stream handler, so we dont see all other libraries log messages in the console. .log file contains all messages.

--- utils/logger.py
+++ utils/logger.py
@@ -3,12 +3,20 @@ import logging
 import os
 
 logging.basicConfig(
-    format='%(levelname)s: "%(asctime)s - %(message)s',
     filename="nvidia-bot.log",
     level=logging.DEBUG,
+    format='%(levelname)s: "%(asctime)s - %(message)s',
 )
 
-LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
-coloredlogs.install(level=LOGLEVEL, fmt='%(levelname)s: "%(asctime)s - %(message)s')
-
 log = logging.getLogger("nvidia-bot")
+log.setLevel(logging.DEBUG)
+
+LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(
+    logging.Formatter('%(levelname)s: "%(asctime)s - %(message)s')
+)
+
+log.addHandler(stream_handler)
+
+coloredlogs.install(LOGLEVEL, logger=log)
