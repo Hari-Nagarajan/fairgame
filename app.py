6e5d875b9b7ee143b1b7b93cecc348431ab58ea1
Worst Mod Confirmed <wmc@privmail.me>
2020-11-21 15:32:17 -0600

    SIGINT is trapping, but webdriver.quit() isn't happening.

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from cli import cli
 from signal import signal, SIGINT
 from utils.logger import log
+
 from selenium import webdriver
+from stores.amazon import Amazon
+
 
 def handler(signal, frame):
     log.info("Caught a ctrl-c, exiting...")
     try:
-        webdriver.Chrome.quit()
-    except Exception:
-        pass
+        webdriver.quit(Amazon(driver))
+    except Exception as e:
+        log.info(e)
+        log.info("Failed to close driver")
     exit(0)
+
 
 if __name__ == "__main__":
     signal(SIGINT, handler)
