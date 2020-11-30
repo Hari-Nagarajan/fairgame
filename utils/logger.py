5dbdf636511a3f3c69d95ad0335d0a04907c3d28
unapproachable <tt_zed@hotmail.com>
2020-11-30 11:28:34 -0500

    Implemented log rolling on startup.

--- utils/logger.py
+++ utils/logger.py
@@ -2,8 +2,27 @@ import coloredlogs
 import logging
 import os
 
+from logging import handlers
+
+LOG_FILENAME = "nvidia-bot.log"
+
+# This check *must* be executed before logging.basicConfig because, at least on Windows,
+# basicConfig creates a lock on the log file that prevents renaming.  Possibly a workaround
+# but putting this first seems to dodge the issue
+if os.path.isfile(LOG_FILENAME):
+    # Create a transient handler to do the rollover for us on startup.  This won't
+    # be added to the logger as a handler... just used to roll the log on startup.
+    rollover_handler = handlers.RotatingFileHandler(LOG_FILENAME, backupCount=10)
+    # Prior log file exists, so roll it to get a clean log for this run
+    try:
+        rollover_handler.doRollover()
+    except Exception:
+        # Eat it since it's *probably* non-fatal and since we're *probably* still able to log to the prior file
+        pass
+
+
 logging.basicConfig(
-    filename="nvidia-bot.log",
+    filename=LOG_FILENAME,
     level=logging.DEBUG,
     format='%(levelname)s: "%(asctime)s - %(message)s',
 )
