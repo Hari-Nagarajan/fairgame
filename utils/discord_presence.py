e9652296ccbac78006bffadda04adc8a1aa23074
Worst Mod Confirmed <wmc@privmail.me>
2020-12-08 16:13:43 -0600

    Fixup for discord handling on linux (and possibly osx).

--- utils/discord_presence.py
+++ utils/discord_presence.py
@@ -8,15 +8,13 @@ from utils.version import version
 start_time = int(time.time())
 client_id = "783592971903696907"
 available = False
+RPC = Presence(client_id=client_id)
+
 try:
-    RPC = Presence(client_id=client_id)
     RPC.connect()
     available = True
-except InvalidPipe:
-    log.warning(
-        "Attempted to connect to your local Discord client to update status, but Discord wasn't available.  "
-        "Is it running?"
-    )
+except:
+    pass
 
 
 def start_presence(status):
