52bb63775220f11d87bbca084299d6a1cce1d198
unapproachable <tt_zed@hotmail.com>
2021-01-05 16:34:32 -0500

    -- Corrected previous commit to explicitly declared sel_exceptions.WebDriverException in cases where a general error has occured with the web driver.

--- stores/amazon.py
+++ stores/amazon.py
@@ -1122,7 +1122,7 @@ class Amazon:
             self.webdriver_child_pids.append(child.pid)
 
     def get_page(self, url):
-        check_cart_element = []
+        check_cart_element = None
         current_page = []
         try:
             check_cart_element = self.driver.find_element_by_xpath(
