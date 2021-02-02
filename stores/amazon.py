a6d865ba0fce7216283ed7241a0771328590cc2d
unapproachable <tt_zed@hotmail.com>
2021-02-02 09:05:05 -0500

    --Hot fix to quickly skip doggo page if we detect it

--- stores/amazon.py
+++ stores/amazon.py
@@ -519,11 +519,14 @@ class Amazon:
             # Sanity check to see if we have any offers
             try:
                 # Wait for the page to load before determining what's in it by looking for the footer
-                footer = WebDriverWait(self.driver, timeout=DEFAULT_MAX_TIMEOUT).until(
+                footer: WebElement = WebDriverWait(self.driver, timeout=DEFAULT_MAX_TIMEOUT).until(
                     lambda d: d.find_elements_by_xpath(
-                        "//div[@class='nav-footer-line']"
+                        "//div[@class='nav-footer-line'] | //img[@alt='Dogs of Amazon']"
                     )
                 )
+                if footer and footer[0].tag_name == "img":
+                    log.info(f"Saw dogs for {asin}.  Skipping...")
+                    return False
 
                 log.debug(f"After footer page title {self.driver.title}")
                 log.debug(f"             page url: {self.driver.current_url}")
