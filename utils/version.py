e32add32970fc3a9655dccd8c9e3d5f828df0b75
DakkJaniels <6080734+DakkJaniels@users.noreply.github.com>
2021-03-28 21:04:33 -0400

    Update version.py
    update to dev2

--- utils/version.py
+++ utils/version.py
@@ -31,7 +31,7 @@ _LATEST_URL = "https://api.github.com/repos/Hari-Nagarajan/fairgame/releases/lat
 # See https://www.python.org/dev/peps/pep-0440/ for specification
 # See https://www.python.org/dev/peps/pep-0440/#examples-of-compliant-version-schemes for examples
 
-__VERSION = "0.7.0.dev1"
+__VERSION = "0.7.0.dev2"
 version = Version(__VERSION)
 
 
