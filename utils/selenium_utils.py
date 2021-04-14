e41af4a22517f102b7b718b6bbb15fa034596a30
DakkJaniels <6080734+DakkJaniels@users.noreply.github.com>
2021-04-14 13:33:16 -0400

    Update selenium_utils.py

--- utils/selenium_utils.py
+++ utils/selenium_utils.py
@@ -156,3 +156,4 @@ def enable_headless():
     options.add_argument("--window-size=1920x1080")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
