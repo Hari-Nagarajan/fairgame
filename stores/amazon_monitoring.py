485af97998c698bbf767f01cee3622191cb93f95
hwamil <hwamil@gmail.com>
2021-05-21 05:19:24 +0900

    Revert "preload proxy group to get rid of ramp up"
    This reverts commit fda822d3a8edaf433407af2bc1f41c67d68b8e80.

--- stores/amazon_monitoring.py
+++ stores/amazon_monitoring.py
@@ -182,7 +182,6 @@ class AmazonMonitor(aiohttp.ClientSession):
     total_groups = 0
     current_group = 1 
     current_group_proxies = set()
-    proxies_loaded = False
 
     def __init__(
         self,
@@ -222,7 +221,6 @@ class AmazonMonitor(aiohttp.ClientSession):
             cls.start_time = time.time()
             cls.current_group_proxies.clear()
             bpc.collection.clear()
-            cls.proxies_loaded = False
 
     @classmethod
     def get_group_total(cls):
@@ -280,22 +278,16 @@ class AmazonMonitor(aiohttp.ClientSession):
             if self.group_num is not self.get_current_group():
                 await asyncio.sleep(600)
                 continue
-            while not self.proxies_loaded:
-                group_length = len(get_proxies(path=PROXY_FILE_PATH)[self.current_group])
-                added = 0
-                while added < group_length:
-                    self.current_group_proxies.add(self.connector.proxy_url)
-                    added+=1
-                self.proxies_loaded = True
+            self.current_group_proxies.add(self.connector.proxy_url)
             good_proxies = self.get_group_total() - bpc.bad_proxies
             stagger_time = delay / good_proxies
             log.debug(f"PROXIES :: GROUP[{self.current_group}] :: GOOD={good_proxies} :: BAD={bpc.bad_proxies} :: Current stagger delay is {round(stagger_time, 2)}s")
             diff = time.time() - self.get_last_task()
+            self.set_last_task()
             if diff < stagger_time:
                 rest_time = stagger_time - diff
                 log.debug(f"Resting for {round((rest_time * 1000), 2)}ms")
-                time.sleep(rest_time)
-            self.set_last_task()
+                await asyncio.sleep(rest_time)
 
             try:
                 log.debug(
@@ -338,7 +330,7 @@ class AmazonMonitor(aiohttp.ClientSession):
                     if qualified_seller:
                         log.debug("Found an offer which meets criteria")
                         if time.time() > self.block_purchase_until:
-                            await queue.put(qualified_seller)
+                            await queue.put_nowait(qualified_seller)
                             log.debug("Offer placed in queue")
                             log.debug("Quitting monitoring task")
                             future.set_result(None)
