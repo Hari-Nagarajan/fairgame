0957b32cefafb9bc30fd8f43ce2c592a010aa314
hwamil <hwamil@gmail.com>
2021-05-22 06:43:02 +0900

    lol coffeebeans freaked me out

--- stores/amazon_monitoring.py
+++ stores/amazon_monitoring.py
@@ -80,7 +80,8 @@ AMAZON_URLS = {
     "PYO_POST": "https://{domain}/gp/buy/spc/handlers/static-submit-decoupled.html/ref=ox_spc_place_order?",
 }
 
-TEST_OFFERID = "7On6yp6oyBHAQdiIZ%2FoOaBwX2XF%2FFgAqRcGzZhDed%2BI11UVFqdR9aYULMUDvfF%2FASutY2lKvuFB91NY7RDQH2jRP6bLHRLKpBpxEEWc7xXFw%2FSV40W5Z9w3m1tq1GgsO%2FiQmjLLKNY%2B1FIwRiH57iw%3D%3D"
+
+TEST_OFFERID = "Aweg8kHVQz3F9Zo%2FvQprIWf7YDM6Ohrgqa7pJ7G9vmjCKrx3Y3V4tvLiVgSzvz5cBOC51koNq5IvvVM0d6mFpRRiRyCn4y%2Fsw3c4SHVHgz6RT0z04L6L8TWqpe3zZ5m4BXmvkP%2BktCfHnMIwUvn0%2Bw%3D%3D"
 COOKIE_HARVEST_URL = "https://www.amazon.com/gp/overlay/display.html"
 OFFERID_PATH = "config/offerid.json"
 
