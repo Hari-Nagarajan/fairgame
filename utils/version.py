4f276597fd1b2dfe78a82d118ac542b35ced38a7
DakkJaniels <6080734+DakkJaniels@users.noreply.github.com>
2021-05-25 17:54:54 -0400

    Update version.py
    update version for hotfix

--- utils/version.py
+++ utils/version.py
@@ -29,7 +29,7 @@ _LATEST_URL = "https://api.github.com/repos/Hari-Nagarajan/fairgame/releases/lat
 # See https://www.python.org/dev/peps/pep-0440/ for specification
 # See https://www.python.org/dev/peps/pep-0440/#examples-of-compliant-version-schemes for examples
 
-__VERSION = "0.6.8"
+__VERSION = "0.6.9"
 version = Version(__VERSION)
 
 
