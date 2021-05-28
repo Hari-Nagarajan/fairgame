427849d9db620a5c41962e6bf120e6007efe3ef7
JackMechem <mechemjack@gmail.com>
2021-05-28 00:22:30 -0700

    Added start and installation sh scripts for unix based systems.

--- install-linux.sh
+++ install-linux.sh
@@ -1,9 +1,9 @@
 YELLOW='\033[1;33m'
-RED='\033[0;32m'
+GREEN='\033[0;32m'
 NC='\033[0m'
 
 if pyenv --version ; then
-
+    printf "${GREEN}You have 'pyenv' installed!${NC} \n"
     else
 
     printf "\n${YELLOW}You do not have 'pyenv' installed.
@@ -15,7 +15,7 @@ fi
 python -m pip install pipenv
 python -m pipenv install
 
-echo -e "\n${RED}------===== Installed! You can now use the launchers!=====------${NC} \n"
+echo -e "\n${GREEN}------===== Installed! You can now use the launchers!=====------${NC} \n"
 
 exit
 
